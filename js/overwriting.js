404d5ed7890ea7c2d0ef29d543e4c1da57e5e4df
Guillaume P <guillaume.poncet.pro@gmail.com>
2017-06-08 09:26:05 +0200

    ajout du projet

---
+++ js/overwriting.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    var navbar = $('nav.rd-navbar--is-clone');
+    var logoSmall = navbar.find('#logo-small');
+    var logoText = navbar.find('#logo-text');
+
+
+    logoSmall.hide();
+    logoText.show();
+    
+});
