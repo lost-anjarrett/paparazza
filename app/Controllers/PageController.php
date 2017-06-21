d884d55af76129057b9e61f799b81e58f0fa5ffe
0-Yorick-0 <yorickferlin@gmail.com>
2017-06-21 16:11:04 +0200

    finalisation du slider en front

--- app/Controllers/PageController.php
+++ app/Controllers/PageController.php
@@ -3,6 +3,7 @@
 namespace Projet\Controllers;
 
 use Projet\Models\Info;
+use Projet\Models\SliderImg;
 
 class PageController extends \System\Controller
 {
@@ -12,6 +13,8 @@ class PageController extends \System\Controller
     public function index()
     {
         $infos = (new Info)->findOneBy('id', 1);
+
+        $slides = (new SliderImg)->findAll();
 
         ob_start();
 
